34dc55ec2691013237b829df3001b94d82985d7c
Leo G Dion <leogdion@brightdigit.com>
2014-03-22 12:31:42 -0400

    load file works

--- bin/nimport.js
+++ bin/nimport.js
@@ -1,5 +1,8 @@
 #!/usr/bin / env node
 
-var nimport = require('../lib/nimport');
+var nimport = require('../lib/nimport'),
+    minimist = require('minimist');
 
-console.log(nimport());
+var nim = nimport.load(minimist(process.argv.slice(2))._, {
+  reporter: "console"
+});
--- test/nimport.js
+++ test/nimport.js
@@ -1,4 +1,24 @@
-exports.helloWorld = function (test) {
-  test.ok(true);
-  test.done();
+var nimport = require("../lib/nimport.js");
+
+var req = function (path) {
+  if (path === "validFile") {
+    return {};
+  } else {
+    throw {
+      code: "MODULE_NOT_FOUND"
+    };
+  }
+};
+
+exports.loadFile = {
+  validFile: function (test) {
+    var nim = nimport.loadFile("validFile", {}, req);
+    test.strictEqual(nim.error, undefined);
+    test.done();
+  },
+  invalidFile: function (test) {
+    var nim = nimport.loadFile("invalidFile", {}, req);
+    test.strictEqual(nim.error.code, "FILE_NOT_FOUND");
+    test.done();
+  }
 };
