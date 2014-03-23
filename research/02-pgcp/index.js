0775606c6124ee5d459afa6319ffcdf037117999
Leo Dion <leogdion@brightdigit.com>
2014-03-23 19:33:56 -0400

    working on transforming tsv to csv

--- research/02-pgcp/index.js
+++ research/02-pgcp/index.js
@@ -2,7 +2,32 @@ var pg = require('pg');
 var copyFrom = require('pg-copy-streams').from;
 var request = require('request');
 var multiline = require('multiline');
+var csv = require("fast-csv");
+/*
+var pgformatter = function (options) {
+  console.log(this);
+  // allow use without new
+  if (!(this instanceof pgformatter)) {
+    return new pgformatter(options);
+  }
+  options = options || {};
+  options.objectMode = true;
 
+  // init Transform
+  Transform.call(this, options);
+};
+
+util.inherits(pgformatter, Transform);
+
+pgformatter.prototype._transform = function (chunk, enc, cb) {
+  var keys = Object.keys(chunk);
+  console.log(key);
+
+  //var upperChunk = chunk.toString();
+  this.push(chunk, enc);
+  cb();
+};
+*/
 //pg.connect("postgres://hoxcsbmwyabkgj:nExK-2u-lH5P4MD_NJncWBkYmP@ec2-107-20-214-225.compute-1.amazonaws.com/d223u8de66gvu4?ssl=true",
   pg.connect("postgres://nimr02_user@localhost/nimr02",
   function (err, client, done) {
@@ -24,11 +49,15 @@ var multiline = require('multiline');
     client.query(query, function (err, results) {
       if (!err) {
         var req = request("http://download.bls.gov/pub/time.series/ap/ap.series");
+        var pgstream = copyFrom("COPY ap_series FROM STDIN");
+        var fcsv = csv({delimiter : '\t', trim: true, headers : true});
+        var ocsv = csv({headers : true})
+        //var formatter = pgformatter();
+        req.pipe(fcsv).pipe(ocsv).pipe(process.stdout);
+        pgstream.on('end', done);
+        pgstream.on('error', done);
+        pgstream.on('data', function (chunk) {
 
-        req.pipe(copyFrom("COPY ap_series FROM STDIN"));
-        req.on('end', done);
-        req.on('error', done);
-        req.on('data', function (chunk) {
           console.log('got %d bytes of data', chunk.length);
         });
       } else {
