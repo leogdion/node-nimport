4e29d32017abd21ec7a86842ecbe13dab9cce95c
Leo Dion <leogdion@brightdigit.com>
2014-04-05 09:27:58 -0400

    added code coverage

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,31 @@ var mocha = require('gulp-mocha');
 var bump = require('gulp-bump');
 var jshint = require('gulp-jshint');
 var beautify = require('gulp-beautify');
+var istanbul = require("gulp-istanbul");
+var coverageEnforcer = require("gulp-istanbul-enforcer");
 
-gulp.task('default', ['beautify', 'lint', 'test', 'bump']);
+gulp.task('default', ['beautify', 'lint', 'test', 'enforce-coverage', 'bump']);
 
-gulp.task('test', function () {
-  gulp.src("./test/**/*.js").pipe(mocha({
-    reporter: 'nyan'
-  }));
+gulp.task('enforce-coverage', function () {
+  var options = {
+    thresholds: {
+      statements: 50,
+      branches: 50,
+      lines: 50,
+      functions: 50
+    },
+    coverageDirectory: 'coverage',
+    rootDirectory: ''
+  };
+  return gulp.src('.').pipe(coverageEnforcer(options));
+});
+
+gulp.task('test', function (cb) {
+  gulp.src(['./lib/**/*.js', './bin/**/*', './bin/**/*.js']).pipe(istanbul()) // Covering files
+  .on('end', function () {
+    gulp.src(["./test/**/*.js"]).pipe(mocha()).pipe(istanbul.writeReports()) // Creating the reports after tests runned
+    .on('end', cb);
+  });
 });
 
 gulp.task('bump', function () {
