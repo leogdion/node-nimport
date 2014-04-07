d73bad5474c2166a160c5068de5b46a988e4048a
Leo Dion <leogdion@brightdigit.com>
2014-04-06 21:53:36 -0400

    fixed unnesscary file

--- test/databases/postgres/queries.js
+++ test/databases/postgres/queries.js
@@ -1,33 +1,5 @@
-/*
-module.exports = function () {
-  return {
-    table: {
-      create: function (table) {
-        var _ = "DROP TABLE IF EXISTS " + table.name + "; CREATE TABLE " + table.name + " (\n";
-        _ += Object.keys(table.columns).map(function (value) {
-          var column = table.columns[value];
-          return "\t" + [value, types(column.type).pgtype(), column.nullable ? "NULL" : "NOT NULL"].join(" ");
-        }).join(",\n");
-        if (table.primaryKey) {
-          _ += S(",\n\tCONSTRAINT {{primaryKey}}_pkey PRIMARY KEY ({{primaryKey}})").template(table);
-        }
-        _ += "\n);";
-        return _;
-      },
-      stream: function (table) {
-        return "COPY " + table.name + " FROM STDIN with CSV";
-      }
-    },
-    database: {
-      create: function (database) {
-        return "CREATE ROLE " + database.user + " LOGIN PASSWORD '" + database.password + "' NOSUPERUSER INHERIT NOCREATEDB NOCREATEROLE NOREPLICATION; CREATE DATABASE " + database.database + " WITH OWNER " + database.user + ";";
-      }
-    }
-  };
-}();
-*/
 var should = require('chai').should();
-var queries = require('__');
+var queries = require('../../../lib/databases/postgres/queries');
 
 describe('queries', function () {
   describe("#table", function () {
